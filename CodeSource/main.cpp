39335a4e0f779955006dc6b2f6673f56873f0c8a
AnUnknown <An_Unknown@live.fr>
2012-05-12 15:48:45 +0200

    'Code_Uploaded'

---
+++ CodeSource/main.cpp
@@ -0,0 +1,90 @@
+#include <cstdlib>
+#include <iostream>
+#include <string>
+#include <conio.h>
+#include "nxt.h"
+using namespace std;
+
+//set up the NXT
+Connection *connection = new Bluetooth();
+Sensor *sensor1 = new Touch(IN_1, connection);
+Sensor *sensor2 = new Touch(IN_3, connection);
+Sensor *sensorSon = new Sonar(IN_2, connection, METRIC);
+Motor *motorA = new Motor(OUT_A, connection);
+Motor *motorC = new Motor(OUT_C, connection);
+Motor *motorB = new Motor(OUT_B, connection);
+int main()
+{
+  try{
+    cout << "Try to connect to the NXT" << endl;
+    connection->connect(4);
+    cout << "Connected" << endl;
+    cout << "Read the touch sensor - hit any key to end" << endl;
+        motorA->stop();
+        motorA->reset_rotation();
+        motorB->stop();
+        motorB->reset_rotation();
+        motorC->stop();
+        motorC->reset_rotation();
+        motorB->get_rotation();
+        long maxRightSA = 0;
+        long maxLeftSA = 0;
+        long middle = 0;
+       /* cout << "calibrer middle" << endl;
+        system("PAUSE");
+        middle = motorB->get_rotation();
+        motorB->off(true);
+
+        cout << "calibrer maxRightSA" << endl;
+        system("PAUSE");
+        maxRightSA = motorB->get_rotation();
+        motorB->off(true);
+
+        cout << "calibrer maxLeftSA" << endl;
+        system("PAUSE");
+        maxLeftSA = motorB->get_rotation();
+        motorB->off(true);
+
+        cout << "maxLeftSA : " << maxLeftSA << endl;
+        motorB->move_to(40,maxLeftSA,6);
+        Sleep(2);
+        cout << "maxRightSA : " << maxRightSA << endl;
+        motorB->move_to(40,maxRightSA,6);
+        Sleep(2);
+        cout << "middle : " << middle << endl;
+        motorB->move_to(40,middle,6);
+        Sleep(2);*/
+       while(!_kbhit())
+             {
+                 if((sensor1->read()==0&&sensor2->read()==0)&&(sensorSon->read()>=40))
+                 {
+                    motorC->on(100); //make motor A turn backwards with speed 85
+                    motorA->on(100); //make motor A turn backwards with speed 85$
+                  //  cout << sensorSon->print() << endl; //returns a string can be used on all sensor types
+                    cout << sensorSon->read() << endl;
+                 }else{
+                    motorC->stop(true);
+                    motorA->stop(true);
+                    cout << sensorSon->read() << endl;
+                 }
+   //   sensor1->read() == 1
+      //cout << sensor1->print() << endl;
+            }
+
+  }
+
+  catch (Nxt_exception& e){
+    //some error occurred - print it out
+    cout << e.what() << endl;
+    cout << "error code: " << e.error_code() << endl;
+    cout << "error type: " << e.error_type() << endl;
+    cout << e.who() << endl;
+    connection->disconnect();
+  }
+    motorC->off(false);
+    motorA->off(false);
+    motorB->off(false);
+    connection->disconnect();
+
+  return 0;
+}
